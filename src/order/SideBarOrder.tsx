cbc9616005dcd18b43ef6c837519c267e722cd43
bicky <bickyrajkayastha@gmail.com>
2025-01-12 13:54:05 +0100

    updates

--- src/order/SideBarOrder.tsx
+++ src/order/SideBarOrder.tsx
@@ -9,7 +9,7 @@ const SideBarOrder: React.FC = () => {
                 <div className="py-7 px-4">
                     <table className="min-w-full text-left border-spacing-4">
                         <thead>
-                        <tr className="border-b border-gray-200 text-gray-900">
+                            <tr className="border-b border-gray-200 text-gray-900">
                             <th className="w-1/12 text-sm/6 font-medium pb-2 mb-2">
                                 Sn
                             </th>
@@ -28,47 +28,52 @@ const SideBarOrder: React.FC = () => {
                         </tr>
                         </thead>
                         <tbody>
-                        <tr className="border-b border-gray-200 text-gray-900">
-                            <td className="pt-2 pb-2 text-sm/6">1</td>
-                            <td className="pt-2 pb-2">
-                                <div className="flex min-w-0 gap-x-1">
-                                    <img alt="" src={food10}
-                                         className="size-7 rounded-md flex-none bg-gray-50"/>
-                                    <div className="min-w-0 flex justify-center items-center">
-                                        <p className="text-xs leading-[1.2] font-medium text-gray-900">Ramen</p>
+                            <tr className="border-b border-gray-200 text-gray-900">
+                                <td className="pt-2 pb-2 text-sm/6">1</td>
+                                <td className="pt-2 pb-2">
+                                    <div className="flex min-w-0 gap-x-1">
+                                        <img alt="" src={food10}
+                                             className="size-7 rounded-md flex-none bg-gray-50"/>
+                                        <div className="min-w-0 flex justify-center items-center">
+                                            <p className="text-xs leading-[1.2] font-medium text-gray-900">Ramen</p>
+                                        </div>
                                     </div>
-                                </div>
-                            </td>
-                            <td className="pt-2 pb-2">
-                                <div className="flex text-sm/6 font-medium text-gray-900 justify-end">x 5</div>
-                            </td>
-                            <td className="pt-2 pb-2">
-                                <p className="text-sm/6 font-medium text-gray-900 text-right">70.99</p>
-                            </td>
-                            <td className="pt-2 pb-2">
-                                <p className="text-sm/6 font-medium text-gray-900 text-right">800.99</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="pt-2 pb-2 text-sm/6">2</td>
-                            <td className="pt-2 pb-2">
-                                <div className="flex min-w-0 gap-x-1">
-                                    <img alt="" src={food10}
-                                         className="size-7 flex-none rounded-md bg-gray-50"/>
-                                    <div className="min-w-0 flex justify-center items-center">
-                                        <p className="text-xs leading-[1.2] font-medium text-gray-900">Sushi Saumon</p>
+                                </td>
+                                <td className="pt-2 pb-2">
+                                    <div className="flex text-sm/6 font-medium text-gray-900 justify-end">x 5</div>
+                                </td>
+                                <td className="pt-2 pb-2">
+                                    <p className="text-sm/6 font-medium text-gray-900 text-right">70.99</p>
+                                </td>
+                                <td className="pt-2 pb-2">
+                                    <p className="text-sm/6 font-medium text-gray-900 text-right">800.99</p>
+                                </td>
+                            </tr>
+                            <tr className="border-b border-gray-200 text-gray-900">
+                                <td className="pt-2 pb-2 text-sm/6">2</td>
+                                <td className="pt-2 pb-2">
+                                    <div className="flex min-w-0 gap-x-1">
+                                        <img alt="" src={food10}
+                                             className="size-7 flex-none rounded-md bg-gray-50"/>
+                                        <div className="min-w-0 flex justify-center items-center">
+                                            <p className="text-xs leading-[1.2] font-medium text-gray-900">Sushi Saumon</p>
+                                        </div>
                                     </div>
-                                </div>
-                            </td>
-                            <td className="pt-2 pb-2">
-                                <div className="flex text-sm/6 font-medium text-gray-900 justify-end">x 5</div>
-                            </td>
-                            <td className="pt-2 pb-2">
-                                <p className="text-sm/6 font-medium text-gray-900 text-right">70.99</p>
-                            </td>
-                            <td className="pt-2 pb-2"><p
-                                className="text-sm/6 font-medium text-gray-900 text-right">50.99</p></td>
-                        </tr>
+                                </td>
+                                <td className="pt-2 pb-2">
+                                    <div className="flex text-sm/6 font-medium text-gray-900 justify-end">x 5</div>
+                                </td>
+                                <td className="pt-2 pb-2">
+                                    <p className="text-sm/6 font-medium text-gray-900 text-right">70.99</p>
+                                </td>
+                                <td className="pt-2 pb-2"><p
+                                    className="text-sm/6 font-medium text-gray-900 text-right">50.99</p></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td colSpan={3} className="pt-2 pb-2 text-sm/6 font-semibold col-span-4">Total</td>
+                                <td className="pt-2 pb-2 text-sm/6 col-span-1 font-semibold text-right">500</td>
+                            </tr>
                         </tbody>
                     </table>
                 </div>
